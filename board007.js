840c2d574e5611aef2154d97d6f2e5c1695801c6
Shiti Saxena <ssaxena.ece@gmail.com>
2012-10-25 00:36:11 +0530

    Update board007.js

--- board007.js
+++ board007.js
@@ -262,7 +262,7 @@
 		         	try{
 		                	if (eventObj.isApiReady) { 
 		                   		startApp(); 
-						gapi.hangout.data.submitDelta( {'board': canvasO.getImageData()} );
+						gapi.hangout.data.submitDelta( {'board': canvasO.getImageData(0,0,height,width)} );
 						gapi.hangout.data.onStateChanged(function (event ) {
 							console.log(event);
 						});
